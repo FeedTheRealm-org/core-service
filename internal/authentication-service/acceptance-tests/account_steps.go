5c51aa4789edc6f69909966f19d77cbf15dc51f8
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-10-07 17:28:04 -0300

    acceptence-test: difined steps for account feature

--- internal/authentication-service/acceptance-tests/account_steps.go
+++ internal/authentication-service/acceptance-tests/account_steps.go
@@ -1,42 +1,98 @@
 package acceptance_tests
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/cucumber/godog"
 )
 
-func aSignUpRequestIsMadeWithEmailAndPassword(email, password string) error {
+type response struct {
+	Message string `json:"message"`
+}
+
+func aSignUpRequestIsMadeWithEmailAndPassword(ctx context.Context, email, password string) (context.Context, error) {
+	resp, err := http.Get("http://0.0.0.0:8000/auth/example-query")
+	if err != nil {
+		return ctx, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ctx, err
+	}
+
+	return context.WithValue(ctx, testContextKey{}, body), nil
+}
+
+func aSignUpRequestIsMadeWithAnEmptyEmailAndPassword(ctx context.Context, password string) (context.Context, error) {
+	return aSignUpRequestIsMadeWithEmailAndPassword(ctx, "", password)
+}
+
+func aSignUpRequestIsMadeWithEmailAndAnEmptyPassword(ctx context.Context, email string) (context.Context, error) {
+	return aSignUpRequestIsMadeWithEmailAndPassword(ctx, email, "")
+}
+
+func theResponseShouldIndicateSuccess(ctx context.Context) error {
+	val := ctx.Value(testContextKey{})
+	body, ok := val.([]byte)
+	if !ok {
+		return fmt.Errorf("no result found in context")
+	}
+
+	var res response
+	if err := json.Unmarshal(body, &res); err != nil {
+		return fmt.Errorf("failed to parse response: %v", err)
+	}
+
+	if res.Message != "User created successfully" {
+		return fmt.Errorf("unexpected result: %s", res.Message)
+	}
+
 	return nil
 }
 
-func aSignUpRequestIsMadeWithAnEmptyEmailAndPassword(password string) error {
-	return aSignUpRequestIsMadeWithEmailAndPassword("", password)
-}
+func theResponseShouldIncludeAnErrorMessage(ctx context.Context, expectedMsg string) error {
+	val := ctx.Value(testContextKey{})
+	body, ok := val.([]byte)
+	if !ok {
+		return fmt.Errorf("no result found in context")
+	}
 
-func aSignUpRequestIsMadeWithEmailAndAnEmptyPassword(email string) error {
-	return aSignUpRequestIsMadeWithEmailAndPassword(email, "")
-}
+	var res response
+	if err := json.Unmarshal(body, &res); err != nil {
+		return fmt.Errorf("failed to parse response: %v", err)
+	}
 
-func aNewAccountShouldBeCreated() error {
+	if res.Message != expectedMsg {
+		return fmt.Errorf("unexpected result: %s", res.Message)
+	}
+
 	return nil
 }
 
-func theResponseShouldIndicateSuccess() error {
-	return nil
-}
+func anAccountAlreadyExistsWithTheEmail(ctx context.Context, email string) error {
+	val := ctx.Value(testContextKey{})
+	body, ok := val.([]byte)
+	if !ok {
+		return fmt.Errorf("no result found in context")
+	}
 
-func theUserShouldBeAbleToLogInWithThoseCredentials() error {
-	return nil
-}
+	var res response
+	if err := json.Unmarshal(body, &res); err != nil {
+		return fmt.Errorf("failed to parse response: %v", err)
+	}
 
-func theResponseShouldIncludeAnErrorMessage(expectedMsg string) error {
-	return nil
-}
+	expectedMsg := fmt.Sprintf("Account with email %s already exists", email)
 
-func theAccountShouldNotBeCreated() error {
-	return nil
-}
+	if res.Message != expectedMsg {
+		return fmt.Errorf("unexpected result: %s", res.Message)
+	}
 
-func anAccountAlreadyExistsWithTheEmail(email string) error {
 	return nil
 }
 
@@ -44,10 +100,7 @@ func InitializeScenarioForAccount(sc *godog.ScenarioContext) {
 	sc.Step(`^a sign-up request is made with email "([^"]*)" and password "([^"]*)"$`, aSignUpRequestIsMadeWithEmailAndPassword)
 	sc.Step(`^a sign-up request is made with an empty email and password "([^"]*)"$`, aSignUpRequestIsMadeWithAnEmptyEmailAndPassword)
 	sc.Step(`^a sign-up request is made with email "([^"]*)" and an empty password$`, aSignUpRequestIsMadeWithEmailAndAnEmptyPassword)
-	sc.Step(`^a new account should be created$`, aNewAccountShouldBeCreated)
 	sc.Step(`^the response should indicate success$`, theResponseShouldIndicateSuccess)
-	sc.Step(`^the user should be able to log in with those credentials$`, theUserShouldBeAbleToLogInWithThoseCredentials)
 	sc.Step(`^the response should include an error message "([^"]*)"$`, theResponseShouldIncludeAnErrorMessage)
-	sc.Step(`^the account should not be created$`, theAccountShouldNotBeCreated)
 	sc.Step(`^an account already exists with the email "([^"]*)"$`, anAccountAlreadyExistsWithTheEmail)
 }
--- internal/authentication-service/acceptance-tests/features/account.feature
+++ internal/authentication-service/acceptance-tests/features/account.feature
@@ -5,20 +5,17 @@ Feature: User Sign-Up
 
   Scenario: AC-1 User can create an account with valid credentials
     When a sign-up request is made with email "alice@example.com" and password "SecurePass123!"
-    Then a new account should be created
+    Then the response should indicate success
 
   Scenario: AC-2a Show error when email field is empty
     When a sign-up request is made with an empty email and password "Password123!"
     Then the response should include an error message "Email is required"
-    And the account should not be created
 
   Scenario: AC-2b Show error when password field is empty
     When a sign-up request is made with email "charlie@example.com" and an empty password
     Then the response should include an error message "Password is required"
-    And the account should not be created
 
   Scenario: AC-3 Prevent account creation with an existing email
     Given an account already exists with the email "existing@example.com"
     When a sign-up request is made with email "existing@example.com" and password "WonderPass123!"
     Then the response should include an error message "Email is already in use"
-    And the account should not be created
