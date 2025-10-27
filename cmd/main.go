0bf4a5a982fbf9ab00cdda2355d7be3ed4f0a6b2
Agustín Barbalase <agustinbarbalase@gmail.com>
2025-10-27 10:31:15 -0300

    feat: added signal handler for grateful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/FeedTheRealm-org/core-service/config"
 	"github.com/FeedTheRealm-org/core-service/internal/server"
 )
 
+func shutdown(s *server.Server) {
+	s.Shutdown()
+}
+
 func main() {
 	conf := config.CreateConfig()
 	s := server.NewServer(conf)
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGTERM)
+
+	go func() {
+		<-signalChannel
+		close(signalChannel)
+		shutdown(s)
+		os.Exit(0)
+	}()
+
 	err := s.Start()
 	if err != nil {
 		log.Fatalf("Error %v", err)
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"context"
+	"net/http"
+	"strconv"
+
 	"github.com/FeedTheRealm-org/core-service/config"
 	"github.com/FeedTheRealm-org/core-service/internal/router"
 	"github.com/FeedTheRealm-org/core-service/internal/utils/logger"
@@ -9,6 +13,7 @@ import (
 
 type Server struct {
 	conf *config.Config
+	srv  *http.Server
 }
 
 func NewServer(conf *config.Config) *Server {
@@ -17,10 +22,31 @@ func NewServer(conf *config.Config) *Server {
 
 func (s *Server) Start() error {
 	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
 
+	r := gin.Default()
 	router.SetupRouter(r, s.conf)
 
-	logger.GetLogger().Info("Starting server on port 8000")
-	return r.Run("0.0.0.0:8000")
+	s.srv = &http.Server{
+		Addr:    "0.0.0.0:" + strconv.Itoa(s.conf.Server.Port),
+		Handler: r,
+	}
+
+	logger.GetLogger().Info("Starting server on port " + strconv.Itoa(s.conf.Server.Port))
+	if err := s.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) Shutdown() {
+	logger.GetLogger().Info("Shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), s.conf.Server.ShutdownTimeout)
+	defer cancel()
+
+	if err := s.srv.Shutdown(ctx); err != nil {
+		logger.GetLogger().Errorf("Server forced to shutdown: %v", err)
+	} else {
+		logger.GetLogger().Info("Server exited properly")
+	}
 }
