ff7095e26d9d73dfa94026f9b6857b9c8f340dbb
maxogod <maxi18wars@gmail.com>
2025-12-18 19:57:07 -0300

    feat: add new categories to seed

--- scripts/seed_all.sh
+++ scripts/seed_all.sh
@@ -17,4 +17,14 @@ echo -e "1\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_C
 echo -e "2\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
 echo -e "3\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
 
+echo -e "4\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/BodyParts/Hair/Basic" > /dev/null
+echo -e "5\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/BodyParts/Beard/Basic" > /dev/null
+echo -e "6\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/BodyParts/Eyebrows/Basic" > /dev/null
+echo -e "7\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/BodyParts/Eyes/Basic" > /dev/null
+echo -e "8\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/BodyParts/Mouth/Basic" > /dev/null
+
+echo -e "9\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/Equipment/Earrings/Common" > /dev/null
+echo -e "10\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/Equipment/Back/Common" > /dev/null
+echo -e "11\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/Common/Sprites/Equipment/Mask/Common" > /dev/null
+
 echo "Done."
