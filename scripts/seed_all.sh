aabcb8e63df5942f80790f1ce7cd809a6e939800
maxogod <maxi18wars@gmail.com>
2025-12-17 23:14:02 -0300

    refactor: update seed for new sprite categories

--- scripts/seed_all.sh
+++ scripts/seed_all.sh
@@ -13,13 +13,11 @@ cat ./scripts/categories.txt | ./scripts/create_categories.py http://localhost:8
 
 echo "Seeding assets..."
 
-echo -e "1\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/0_Hair" > /dev/null
-echo -e "2\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/0_Eye" > /dev/null
-echo -e "3\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/1_FaceHair" > /dev/null
-echo -e "4\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/2_Cloth" > /dev/null
-echo -e "5\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/3_Pant" > /dev/null
-echo -e "6\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/5_Armor" > /dev/null
-echo -e "7\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/4_Helmet" > /dev/null
-echo -e "8\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/SPUM/Resources/Addons/Legacy/0_Unit/0_Sprite/7_Back" > /dev/null
+echo -e "1\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
+echo -e "2\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
+echo -e "3\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
+echo -e "4\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
+echo -e "5\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
+echo -e "6\npng\ny\n" | ./scripts/upload_assets.py http://localhost:8000 "$FTR_CLIENT_PATH/Assets/HeroEditor4D/FantasyHeroes/Sprites/Equipment/Armor/Basic" > /dev/null
 
 echo "Done."
