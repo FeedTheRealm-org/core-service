818bcca52469bf54643de648e69584b848618a57
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-02-07 15:09:50 -0300

    feat: deleted deprecated items services

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,7 +5,6 @@ import (
 	assetsRouter "github.com/FeedTheRealm-org/core-service/internal/assets-service/router"
 	authRouter "github.com/FeedTheRealm-org/core-service/internal/authentication-service/router"
 	"github.com/FeedTheRealm-org/core-service/internal/common_handlers"
-	itemsRouter "github.com/FeedTheRealm-org/core-service/internal/items-service/router"
 	"github.com/FeedTheRealm-org/core-service/internal/middleware"
 	playersRouter "github.com/FeedTheRealm-org/core-service/internal/players-service/router"
 	"github.com/FeedTheRealm-org/core-service/internal/utils/session"
@@ -36,10 +35,6 @@ func SetupRouter(r *gin.Engine, conf *config.Config, db *config.DB) error {
 	}
 
 	if err := assetsRouter.SetupAssetsServiceRouter(r, conf, db); err != nil {
-		return err
-	}
-
-	if err := itemsRouter.SetupItemsServiceRouter(r, conf, db); err != nil {
 		return err
 	}
 
