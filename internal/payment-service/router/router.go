27c4464363cd718e020d07d22231eac2812874fa
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-03-19 17:15:00 -0300

    feat: added endpoints for gem balances on router

--- internal/payment-service/router/router.go
+++ internal/payment-service/router/router.go
@@ -2,11 +2,11 @@ package router
 
 import (
 	"github.com/FeedTheRealm-org/core-service/config"
-	// gem_balances_controller "github.com/FeedTheRealm-org/core-service/internal/payment-service/controllers/gem-balances"
+	gem_balances_controller "github.com/FeedTheRealm-org/core-service/internal/payment-service/controllers/gem-balances"
 	gem_packs_controller "github.com/FeedTheRealm-org/core-service/internal/payment-service/controllers/gem-packs"
-	// gem_balances_repo "github.com/FeedTheRealm-org/core-service/internal/payment-service/repositories/gem-balances"
+	gem_balances_repo "github.com/FeedTheRealm-org/core-service/internal/payment-service/repositories/gem-balances"
 	gem_packs_repo "github.com/FeedTheRealm-org/core-service/internal/payment-service/repositories/gem-packs"
-	// gem_balances_service "github.com/FeedTheRealm-org/core-service/internal/payment-service/services/gem-balances"
+	gem_balances_service "github.com/FeedTheRealm-org/core-service/internal/payment-service/services/gem-balances"
 	gem_packs_service "github.com/FeedTheRealm-org/core-service/internal/payment-service/services/gem-packs"
 	"github.com/gin-gonic/gin"
 )
@@ -25,29 +25,29 @@ func SetupGemPacksServiceRouter(conf *config.Config, db *config.DB, g *gin.Route
 	packsGroup.DELETE("/:id", gemGemPacksController.DeleteGemPack)
 }
 
-// func SetupBalancesServiceRouter(conf *config.Config, db *config.DB, paymentGroup *gin.RouterGroup, gemsGroup *gin.RouterGroup) {
-// 	gemBalancesRepo := gem_balances_repo.NewGemBalancesRepository(conf, db)
-// 	packsRepo := gem_packs_repo.NewGemPacksRepository(conf, db)
-// 	gemBalancesService := gem_balances_service.NewGemBalancesService(conf, gemBalancesRepo, packsRepo)
-// 	gemBalancesController := gem_balances_controller.NewGemBalancesController(conf, gemBalancesService)
+func SetupBalancesServiceRouter(conf *config.Config, db *config.DB, paymentGroup *gin.RouterGroup, gemsGroup *gin.RouterGroup) {
+	gemBalancesRepo := gem_balances_repo.NewGemBalancesRepository(conf, db)
+	packsRepo := gem_packs_repo.NewGemPacksRepository(conf, db)
+	gemBalancesService := gem_balances_service.NewGemBalancesService(conf, gemBalancesRepo, packsRepo)
+	gemBalancesController := gem_balances_controller.NewGemBalancesController(conf, gemBalancesService)
 
-// 	/* Balances Endpoints */
-// 	balancesGroup := gemsGroup.Group("/balances")
-// 	// balancesGroup.GET("", gemBalancesController.GetAllGemBalances)
-// 	balancesGroup.GET("/", gemBalancesController.GetGemBalanceByUserId)
-// 	balancesGroup.PUT("/:id", gemBalancesController.UpdateGemBalance)
+	/* Balances Endpoints */
+	balancesGroup := gemsGroup.Group("/balances")
+	// balancesGroup.GET("", gemBalancesController.GetAllGemBalances)
+	balancesGroup.GET("/", gemBalancesController.GetGemBalanceByUserId)
+	balancesGroup.PUT("/:id", gemBalancesController.UpdateGemBalance)
 
-// 	/* Webhook Endpoint */
-// 	paymentGroup.POST("/checkout", gemBalancesController.CreateCheckoutSession)
-// 	paymentGroup.POST("/webhook/stripe", gemBalancesController.HandleStripeWebhook)
-// }
+	/* Webhook Endpoint */
+	paymentGroup.POST("/checkout", gemBalancesController.CreateCheckoutSession)
+	paymentGroup.POST("/webhook/stripe", gemBalancesController.HandleStripeWebhook)
+}
 
 func SetupPaymentServiceRouter(r *gin.Engine, conf *config.Config, db *config.DB) error {
 	paymentGroup := r.Group("/payments")
 	gemsGroup := paymentGroup.Group("/gems")
 
 	SetupGemPacksServiceRouter(conf, db, gemsGroup)
-	// SetupBalancesServiceRouter(conf, db, paymentGroup, gemsGroup)
+	SetupBalancesServiceRouter(conf, db, paymentGroup, gemsGroup)
 
 	return nil
 }
