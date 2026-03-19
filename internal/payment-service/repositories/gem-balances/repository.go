70b69f15ca60ceee84baab21327ef8b967e3f901
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-03-19 17:12:46 -0300

    feat: created gem balances repo

---
+++ internal/payment-service/repositories/gem-balances/repository.go
@@ -0,0 +1,14 @@
+package gem_balances
+
+import (
+	"github.com/FeedTheRealm-org/core-service/internal/payment-service/models"
+	"github.com/google/uuid"
+)
+
+type GemBalancesRepository interface {
+	CreateGemBalance(userId uuid.UUID) error
+	GetAllGemBalances() ([]*models.GemBalance, error)
+	GetGemBalanceByUserId(userId uuid.UUID) (*models.GemBalance, error)
+	AddToGemBalance(userId uuid.UUID, gems int) error
+	UpdateGemBalance(userId uuid.UUID, gems int) error
+}
