0005ebcb9be97e0c51951dbcfacee3202665518b
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-03-28 00:26:15 -0300

    feat: added new responses and request for zones

--- internal/world-service/dtos/requests_dto.go
+++ internal/world-service/dtos/requests_dto.go
@@ -2,7 +2,18 @@ package dtos
 
 // WorldDataRequest represents the request payload for updating world information.
 type WorldRequest struct {
-	FileName    string `json:"file_name"`
-	Data        any    `json:"data"`
-	Description string `json:"description,omitempty"`
+	FileName       string `json:"file_name"`
+	Data           any    `json:"data"`
+	CreateableData any    `json:"createable_data,omitempty"`
+	Description    string `json:"description,omitempty"`
+}
+
+type UpdateCreateableDataRequest struct {
+	CreateableData any `json:"createable_data"`
+}
+
+type PublishZoneRequest struct {
+	WorldID string `json:"world_id"`
+	ZoneID  int    `json:"zone_id"`
+	Data    any    `json:"data"`
 }
--- internal/world-service/dtos/responses_dto.go
+++ internal/world-service/dtos/responses_dto.go
@@ -6,13 +6,14 @@ import "time"
 
 // WorldResponse represents the response payload for retrieving world information.
 type WorldResponse struct {
-	ID          string    `json:"id"`
-	UserId      string    `json:"user_id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description,omitempty"`
-	Data        string    `json:"data"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID             string    `json:"id"`
+	UserId         string    `json:"user_id"`
+	Name           string    `json:"name"`
+	Description    string    `json:"description,omitempty"`
+	Data           string    `json:"data"`
+	CreateableData string    `json:"createable_data"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 type WorldsListResponse struct {
@@ -23,15 +24,32 @@ type WorldsListResponse struct {
 }
 
 type WorldMetadata struct {
-	ID          string    `json:"id"`
-	UserId      string    `json:"user_id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description,omitempty"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID             string    `json:"id"`
+	UserId         string    `json:"user_id"`
+	Name           string    `json:"name"`
+	Description    string    `json:"description,omitempty"`
+	CreateableData string    `json:"createable_data"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 type WorldAddressResponse struct {
 	IP   string `json:"ip"`
 	Port int    `json:"port"`
+}
+
+type WorldZonesResponse struct {
+	WorldID string          `json:"world_id"`
+	Zones   []WorldZoneData `json:"zones"`
+}
+
+type WorldZoneData struct {
+	ZoneID   int    `json:"zone_id"`
+	ZoneData string `json:"zone_data"`
+}
+
+type WorldZoneResponse struct {
+	WorldID  string `json:"world_id"`
+	ZoneID   int    `json:"zone_id"`
+	ZoneData string `json:"zone_data"`
 }
