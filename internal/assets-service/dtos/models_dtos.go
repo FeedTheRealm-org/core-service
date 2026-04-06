1f587c51cc2c98b3cf2653f0fb7d9faa9ee72231
sev <santisevitz@hotmail.com>
2026-04-06 15:31:17 -0300

    fix: once again, attempting to fix the models service

--- internal/assets-service/dtos/models_dtos.go
+++ internal/assets-service/dtos/models_dtos.go
@@ -1,8 +1,22 @@
 package dtos
 
 import (
+	"mime/multipart"
+
 	"github.com/google/uuid"
 )
+
+type ModelRequest struct {
+	Id        uuid.UUID             `json:"model_id" binding:"required"`
+	Url       string                `json:"url" binding:"required"`
+	ModelFile *multipart.FileHeader `gorm:"-" json:"-"`
+}
+
+type BatchModelsRequest struct {
+	WorldID   uuid.UUID      `json:"world_id" binding:"required"`
+	CreatedBy uuid.UUID      `json:"created_by" binding:"required"`
+	Models    []ModelRequest `json:"models" binding:"required,dive"`
+}
 
 type ModelResponse struct {
 	ModelID uuid.UUID `json:"model_id"`
