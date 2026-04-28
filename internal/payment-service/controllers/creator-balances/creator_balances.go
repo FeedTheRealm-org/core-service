c59563db7776230e0934b2fb6de5e49bf70da2f1
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-04-28 17:55:57 -0300

    feat: created controller for creator balance

---
+++ internal/payment-service/controllers/creator-balances/creator_balances.go
@@ -0,0 +1,50 @@
+package creator_balances
+
+import (
+	"net/http"
+
+	"github.com/FeedTheRealm-org/core-service/internal/common_handlers"
+	"github.com/FeedTheRealm-org/core-service/internal/errors"
+	"github.com/FeedTheRealm-org/core-service/internal/payment-service/dtos"
+	creator_balances "github.com/FeedTheRealm-org/core-service/internal/payment-service/services/creator-balances"
+	"github.com/gin-gonic/gin"
+)
+
+type creatorBalancesController struct {
+	service creator_balances.CreatorBalancesService
+}
+
+func NewCreatorBalancesController(service creator_balances.CreatorBalancesService) CreatorBalancesController {
+	return &creatorBalancesController{service: service}
+}
+
+// GetBalance godoc
+// @Summary      Get creator balance
+// @Description  Get the current creator balance of the authenticated user
+// @Tags         payment-service
+// @Security     BearerAuth
+// @Produce      json
+// @Success      200  {object}  dtos.CreatorBalanceResponse
+// @Failure      401  {object} dtos.ErrorResponse
+// @Failure      500  {object} dtos.ErrorResponse
+// @Router       /payments/creator-balances [get]
+func (c *creatorBalancesController) GetBalance(ctx *gin.Context) {
+	userId, err := common_handlers.GetUserIDFromSession(ctx)
+	if err != nil {
+		_ = ctx.Error(errors.NewUnauthorizedError(err.Error()))
+		return
+	}
+
+	balance, err := c.service.GetBalance(userId)
+	if err != nil {
+		_ = ctx.Error(err)
+		return
+	}
+
+	res := dtos.CreatorBalanceResponse{
+		UserID:  userId,
+		Balance: balance,
+	}
+
+	common_handlers.HandleSuccessResponse(ctx, http.StatusOK, res)
+}
