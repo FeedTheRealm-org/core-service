dc5fe8a441f2df6551debdca36cdaacc2ce108c6
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-04-29 18:42:13 -0300

    feat: deleted ID for creator balance model

--- internal/payment-service/models/creator_balance.go
+++ internal/payment-service/models/creator_balance.go
@@ -7,8 +7,7 @@ import (
 )
 
 type CreatorBalance struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	UserID    uuid.UUID `gorm:"type:uuid;not null;unique"`
+	UserID    uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Balance   float64   `gorm:"type:numeric(10,2);not null;default:0"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
