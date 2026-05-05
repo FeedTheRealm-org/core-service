ead1b33d9a4904f892e406c888ae6151e2172c75
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-05-05 00:28:12 -0300

    feat: added model for material and error MaterialNotFound

---
+++ internal/assets-service/models/material.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Material struct {
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	WorldID   uuid.UUID `gorm:"type:uuid;not null;index:idx_materials_world_id" json:"world_id"`
+	URL       string    `gorm:"type:text;not null" json:"url"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	CreatedBy uuid.UUID `gorm:"type:uuid;not null" json:"created_by"`
+}
+
+func (Material) TableName() string {
+	return "materials"
+}
