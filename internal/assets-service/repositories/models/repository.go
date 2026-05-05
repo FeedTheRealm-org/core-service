97a26e69808292654933349679e7d24a5ec6ac4d
sev <santisevitz@hotmail.com>
2026-05-05 20:31:20 -0300

    fix duplicate package

--- internal/assets-service/repositories/models/repository.go
+++ internal/assets-service/repositories/models/repository.go
@@ -2,12 +2,11 @@ package models
 
 import (
 	"github.com/FeedTheRealm-org/core-service/internal/assets-service/models"
-	assetModels "github.com/FeedTheRealm-org/core-service/internal/assets-service/models"
 	"github.com/google/uuid"
 )
 
 // ModelsRepository defines the interface for models-related database operations.
 type ModelsRepository interface {
-	UploadModel(model assetModels.Model) (*assetModels.Model, error)
+	UploadModel(model models.Model) (*models.Model, error)
 	GetModelsByWorld(worldId uuid.UUID) ([]models.Model, error)
 }
