0842d9cf20d913e311d3d4e529dd5432f1f89ff4
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-05-05 01:50:25 -0300

    feat: added omitempty for MaterialResponse

--- internal/assets-service/dtos/materials_dtos.go
+++ internal/assets-service/dtos/materials_dtos.go
@@ -14,9 +14,9 @@ type PutMaterialRequest struct {
 
 type MaterialResponse struct {
 	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	WorldID   uuid.UUID `json:"world_id"`
-	URL       string    `json:"url"`
-	CreatedAt string    `json:"created_at"`
-	UpdatedAt string    `json:"updated_at"`
+	Name      string    `json:"name,omitempty"`
+	WorldID   uuid.UUID `json:"world_id,omitempty"`
+	URL       string    `json:"url,omitempty"`
+	CreatedAt string    `json:"created_at,omitempty"`
+	UpdatedAt string    `json:"updated_at,omitempty"`
 }
