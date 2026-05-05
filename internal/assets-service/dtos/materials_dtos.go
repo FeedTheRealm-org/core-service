a042102af5e71c9c9e9d4078fcfa67a171572dcf
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-05-05 00:43:46 -0300

    feat: created DTOs for material service

---
+++ internal/assets-service/dtos/materials_dtos.go
@@ -0,0 +1,21 @@
+package dtos
+
+import (
+	"mime/multipart"
+
+	"github.com/google/uuid"
+)
+
+type PutMaterialRequest struct {
+	WorldID uuid.UUID
+	UserID  uuid.UUID
+	Files   []*multipart.FileHeader
+}
+
+type MaterialResponse struct {
+	ID        uuid.UUID `json:"id"`
+	WorldID   uuid.UUID `json:"world_id"`
+	URL       string    `json:"url"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+}
