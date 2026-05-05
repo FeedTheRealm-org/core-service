fa28ad9f811f72a21dd8019053708459a480e351
Agustín Barbalase <agustinbarbalase@gmail.com>
2026-05-05 00:26:58 -0300

    feat: added migrations for materials table

---
+++ migrations/assets-service/12_create_materials_table.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS materials (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    world_id UUID NOT NULL,
+    url TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    created_by UUID NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_materials_world_id ON materials(world_id);
+
+COMMIT;
